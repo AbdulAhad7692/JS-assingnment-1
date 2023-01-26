4a5115240824cc364b2aa6ffb0a6b0411828a00a
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-01-26 22:56:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// chapter 1  //
+
+alert("WELCOME USER")
+
+
+alert("Erorr! enter a invalid password")
+
+
+alert("Welcome to JS Land\nHappy coding")
+
+
+alert("Welcome to JS Land")
+
+
+alert("Hello ! i can run JS through my web browser console")
+
+
+alert("Using alert in my new html project")
+
+
+// chapter 1 Assignment closed //
